02d0f7e4aa3189fb77f4faee86fb39b47c68f5ba
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-08 18:26:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ Base = declarative_base()
 engine = create_engine("sqlite:///space_game.db")
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+
+# ----------------------------
+# DATABASE MODELS
+# ----------------------------
+
+class Captain(Base):
+    __tablename__ = "captains"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    ship_name = Column(String)
+    fuel = Column(Integer)
+
+    resources = relationship("Resource", back_populates="captain")
+
+
